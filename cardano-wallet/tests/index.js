633b48f99015881595f7ef021fcdc51f2541181c
Nicolas Di Prima <nicolas.diprima@iohk.io>
2019-01-10 23:39:32 +0100

    fix the in browset JS file

--- cardano-wallet/tests/index.js
+++ cardano-wallet/tests/index.js
@@ -12,6 +12,8 @@ Wallet
     const MNEMONICS = "crowd captain hungry tray powder motor coast oppose month shed parent mystery torch resemble index";
     const PASSWORD = "Cardano Rust for the winners!";
 
+    let settings = Wallet.BlockchainSettings.mainnet();
+
     let entropy = Wallet.Entropy.from_english_mnemonics(MNEMONICS);
     let wallet = Wallet.Bip44RootPrivateKey.recover(entropy, PASSWORD);
 
@@ -21,8 +23,8 @@ Wallet
     let key_prv = account.address_key(false, Wallet.AddressKeyIndex.new(0));
     let key_pub = account_public.address_key(false, Wallet.AddressKeyIndex.new(0));
 
-    let address = key_pub.bootstrap_era_address();
+    let address = key_pub.bootstrap_era_address(settings);
 
-    console.log("Address m/bip44/ada/'0/0/0", address.to_hex());
+    console.log("Address m/bip44/ada/'0/0/0", address.to_base58());
   })
   .catch(console.error);
